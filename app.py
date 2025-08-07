cf3fad486bc67502902d035867be53386d85019d
louisludev <louis.lu.company@gmail.com>
2025-08-08 07:44:20 +0800

    practice_0808

---
+++ app.py
@@ -0,0 +1,13 @@
+# C = input("請輸入攝氏溫度°C:")
+# print(f"華氏溫度為{float(C)*9/5+32:.1f}°F")
+
+# patient_name = "Jhon Smith"
+# patient_age = 20
+# new_patient = True
+
+# name = input("What is your name? ")
+# color = input("What is your favorite color? ")
+# print(f"Hi! {name}. your favorite color is {color}, It's cool!")
+
+kg = input("Please type your weight(kg): ")
+print(f"your weight is {float(kg)*2.2:.1f}lb.")
